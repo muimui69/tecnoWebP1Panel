55625c02f7d9a59ff59da487bc2af6095c2077e9
muimui69 <moises_c2015@outlook.es>
2025-02-08 08:46:45 -0400

    completo

--- src/app/services/GLOBAL.ts
+++ src/app/services/GLOBAL.ts
@@ -1,6 +1,7 @@
 export const GLOBAL = {
     // url: 'https://tecnowebp1backend.onrender.com',
-    url: 'http://localhost:3000',
+    // url: 'http://localhost:3000',
+    url: 'https://7476-190-129-198-45.ngrok-free.app',
     tallas: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
     colores: ['Azul', 'Rojo', 'Verde', 'Amarillo', 'Negro', 'Blanco', 'Naranja', 'Cafe', 'Rosado'],
     proovedores: ['ZARA', 'ARMANI', 'HERMES', 'SHEIN'],
