561ef1194fa7e499438f34036aaec2a2af320425
muimui69 <moises_c2015@outlook.es>
2025-02-08 07:39:15 -0400

    completo

--- src/app/services/reporte.service.ts
+++ src/app/services/reporte.service.ts
@@ -22,8 +22,14 @@ export class ReporteService {
         return this._http.get(this.url + `/reporte/pdf?entidad=${entidad}&inicio=${inicio}&fin=${fin}`, { headers: headers, responseType: 'blob' });
     }
 
+    // enviarReporteCorreo(entidad: string, inicio: string, fin: string, emails: string, subject: string, body: string, token: any): Observable<any> {
+    //     const headers = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': token });
+    //     return this._http.post(this.url + `/reporte/enviar?entidad=${entidad}&inicio=${inicio}&fin=${fin}&emails=${emails}&subject=${subject}&body=${body}`, {}, { headers: headers });
+    // }
+
     enviarReporteCorreo(entidad: string, inicio: string, fin: string, emails: string, subject: string, body: string, token: any): Observable<any> {
         const headers = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': token });
-        return this._http.post(this.url + `/reporte/enviar?entidad=${entidad}&inicio=${inicio}&fin=${fin}&emails=${emails}&subject=${subject}&body=${body}`, {}, { headers: headers });
+        const payload = { entidad, inicio, fin, emails, subject, body };
+        return this._http.post(this.url + '/reporte/enviar', payload, { headers: headers });
     }
 }
