88f840b9c9403b8c7386a924f076769fff44bbb2
muimui69 <moises_c2015@outlook.es>
2025-02-12 02:38:55 -0400

    pago

--- src/app/services/GLOBAL.ts
+++ src/app/services/GLOBAL.ts
@@ -1,7 +1,7 @@
 export const GLOBAL = {
     // url: 'https://tecnowebp1backend.onrender.com',
-    url: 'http://localhost:3000',
-    // url: 'https://tecnowebp1backend-production.up.railway.app',
+    // url: 'http://localhost:3000',
+    url: 'https://tecnowebp1backend-production.up.railway.app',
     // url: 'https://7476-190-129-198-45.ngrok-free.app',
     tallas: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
     colores: ['Azul', 'Rojo', 'Verde', 'Amarillo', 'Negro', 'Blanco', 'Naranja', 'Cafe', 'Rosado'],
